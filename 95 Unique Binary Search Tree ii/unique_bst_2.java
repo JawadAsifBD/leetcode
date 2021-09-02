04fcf5913f0af8a68c1082c448038197e69d2aa1
Md Jawad Noor Asif <jawad.asif.bd@gmail.com>
2021-09-02 21:11:44 +0600

    java accepted

--- 95 Unique Binary Search Tree ii/unique_bst_2.java
+++ 95 Unique Binary Search Tree ii/unique_bst_2.java
@@ -15,33 +15,27 @@
  */
 class Solution {
     public List<TreeNode> generateTrees(int n) {
-        List<TreeNode> res = rec(1,n);
-        return res;
+        return rec(1,n);
     }
     
     public List<TreeNode> rec(int start, int end) {
-        List<TreeNode> res = new ArrayList<>();
-        
-        if(start > end) {
-            return List.of(new TreeNode());
+        List<TreeNode> res = new ArrayList();
+        if ( start > end ) {
+            res.add(null);
+            return res;
         }
-        
-        if (start == end) {
-            return List.of(new TreeNode(start));
-        }
-        
-        
-        for(int i = start; i <= end; i++) {
+        for(int i=start; i<=end; i++) {
             List<TreeNode> left = rec(start,i-1);
             List<TreeNode> right = rec(i+1,end);
-
-
-            for( TreeNode l : left) {
-                for (TreeNode r: right) {
-                    res.add(new TreeNode(i,l,r));
+            
+            for(TreeNode l : left ) {
+                for(TreeNode r : right ) {
+                    TreeNode current = new TreeNode(i);
+                    current.left = l;
+                    current.right = r;
+                    res.add(current);
                 }
             }
-
         }
         return res;
     }
