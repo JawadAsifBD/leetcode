764600182d2d2b1d2ff17c131a75606a1028a6b8
Md Jawad Noor Asif <jawad.asif.bd@gmail.com>
2021-09-09 11:13:25 +0600

    updated for loop

--- 848 shifting letters/shifting_letters.py
+++ 848 shifting letters/shifting_letters.py
@@ -1,27 +1,26 @@
 from typing import List
+
+
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
-        shifts = [i%26 for i in shifts]
+        shifts = [i % 26 for i in shifts]
         # print(shifts)
-        sum1 = 0
-        finalshifts = []
-        for i in shifts[::-1]:
-            finalshifts.append((i+sum1)%26)
-            sum1 += i 
-        finalshifts.reverse()
+        finalshifts = [sum(shifts[i:]) % 26 for i in range(len(shifts))]
         # print(finalshifts)
+
         res = ''
-        for i in range(0,len(finalshifts)):
-            # print((ord(s[i])+finalshifts[i])%26)
-            res += chr((ord(s[i])+finalshifts[i]-ord('a'))%26+ord('a'))
-        
+        for i in range(0, len(finalshifts)):
+            # print((ord(s[i])+finalshifts[i]) % 26)
+            res += chr((ord(s[i])+finalshifts[i]-ord('a')) % 26+ord('a'))
+
         # print(res)
         return res
 
+
 s = Solution()
 str1 = "mkgfzkkuxownxvfvxasy"
-shifts = [505870226,437526072,266740649,224336793,532917782,311122363,\
-    567754492,595798950,81520022,684110326,137742843,275267355,856903962,\
-        148291585,919054234,467541837,622939912,116899933,983296461,536563513]
-        
-s.shiftingLetters(str1,shifts)
+shifts = [505870226, 437526072, 266740649, 224336793, 532917782, 311122363,
+          567754492, 595798950, 81520022, 684110326, 137742843, 275267355, 856903962,
+          148291585, 919054234, 467541837, 622939912, 116899933, 983296461, 536563513]
+
+s.shiftingLetters(str1, shifts)
