4f791cb2b108f28c4686e5a9f69a869838fb8b09
Md Jawad Noor Asif <jawad.asif.bd@gmail.com>
2021-11-16 10:52:40 +0600

    668. Kth Smallest Number in Multiplication Table

---
+++ 668. Kth Smallest Number in Multiplication Table/668. Kth Smallest Number in Multiplication Table.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findKthNumber(self, m: int, n: int, k: int) -> int:
+        def enough(x):
+            count = 0
+            for i in range(1, m+1):
+                count += min(x // i, n)
+            return count >= k
+
+        lo, hi = 1, m * n
+        while lo < hi:
+            mi = (lo + hi) // 2
+            if not enough(mi):
+                lo = mi + 1
+            else:
+                hi = mi
+        return lo
+
+
+m = 9895
+n = 28405
+k = 100787757
+s = Solution()
+print(s.findKthNumber(m, n, k))
