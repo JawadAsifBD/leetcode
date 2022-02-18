98a66b78c3057d00e0a8d376d6e0dea1c4e3fa7f
Md Jawad Noor Asif <jawad.asif.bd@gmail.com>
2022-02-18 23:25:33 +0600

    updated

--- 1155. Number of Dice Rolls With Target Sum/1155. Number of Dice Rolls With Target Sum bottom-up.java
+++ 1155. Number of Dice Rolls With Target Sum/1155. Number of Dice Rolls With Target Sum bottom-up.java
@@ -1,27 +1,4 @@
-import java.util.Arrays;
-
 class Solution {
-    private int[][] memo;
-    private int k, modulo = 1000000007;
-
-    private int dp(int dice_left, int target_left) {
-        if (dice_left == 0) {
-            if (target_left == 0) {
-                return 1;
-            }
-            return 0;
-        }
-        if (memo[dice_left][target_left] == -1) {
-            int res = 0;
-            for (int i = 1; i <= Math.min(k, target_left); i++) {
-                res += dp(dice_left - 1, target_left - i);
-                res %= modulo;
-            }
-            memo[dice_left][target_left] = res;
-        }
-        return memo[dice_left][target_left];
-    }
-
     public int numRollsToTarget(int n, int k, int target) {
         int[][] dp = new int[n + 1][target + 1];
         int modulo = 1000000007;
