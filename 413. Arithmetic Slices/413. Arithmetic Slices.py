97d272bcca0cc3ff8e4bbd4f9191bc6d9c5bd5fb
JawadAsif <jawad.asif.bd@gmail.com>
2022-03-03 10:20:18 +0600

    413. Arithmetic Slices

---
+++ 413. Arithmetic Slices/413. Arithmetic Slices.py
@@ -0,0 +1,29 @@
+from functools import cache
+from typing import List
+
+
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        @cache
+        def count(n):
+            if cnt < 3:
+                return 0
+            return n - 2 + count(n - 1)
+
+        prev, cnt, ans = None, 2, 0
+        for i in range(1, len(nums)):
+            diff = nums[i-1] - nums[i]
+            if diff == prev:
+                cnt += 1
+            else:
+                ans += count(cnt)
+                cnt = 2
+
+            prev = diff
+
+        return ans + count(cnt)
+
+
+s = Solution()
+nums = [1, 2, 3, 4]
+print(s.numberOfArithmeticSlices(nums))
