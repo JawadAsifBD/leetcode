de70c728b473481a877b66375d153d9b1c23a01f
JawadAsif <jawad.asif.bd@gmail.com>
2022-04-03 09:25:00 +0600

    344. Reverse String

--- 344. Reverse String/344. Reverse String.cpp
+++ 344. Reverse String/344. Reverse String.cpp
@@ -1,7 +1,10 @@
-class Solution {
+class Solution
+{
 public:
-    void reverseString(vector<char>& s) {
-	int i = 0, j = s.size() - 1;
-	while (i < j) swap(s[i++], s[j--]);
-}
+	void reverseString(vector<char> &s)
+	{
+		int i = 0, j = s.size() - 1;
+		while (i < j)
+			swap(s[i++], s[j--]);
+	}
 };
