12462bcdcc91895bc8348e6339d57d34cdc62033
JawadAsif <jawad.asif.bd@gmail.com>
2022-05-05 09:11:49 +0600

    225. Implement Stack using Queues

---
+++ 225. Implement Stack using Queues/225. Implement Stack using Queues.java
@@ -0,0 +1,23 @@
+class MyStack {
+    Stack<Integer> st;
+
+    public MyStack() {
+        st = new Stack<>();
+    }
+
+    public void push(int x) {
+        st.push(x);
+    }
+
+    public int pop() {
+        return st.pop();
+    }
+
+    public int top() {
+        return st.peek();
+    }
+
+    public boolean empty() {
+        return st.size() == 0;
+    }
+}
